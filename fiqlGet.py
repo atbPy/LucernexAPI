b696839c0926a722568a64bd9bb8b50e3141929d
adam <adam@bednar.co>
2017-08-20 21:20:25 -0400

    Put the data in a dictionary

--- fiqlGet.py
+++ fiqlGet.py
@@ -9,21 +9,29 @@ fields.read('fields.conf')
 firm = configparser.ConfigParser()
 firm.read('firm.conf')
 
-def get_field(field, env="TRAIN"):
+
+def get_field(field, env="TRAIN", fiql=""):
     data = defaultdict(dict)
 
     headers = {'Authorization': firm["FIRM"]["Token"], 'Content-Type': 'application/xml'}
-    params = {'fiql': fields[field]["FIQL"], 'fields': fields[field]["Fields"]}
+    params = {'fiql': fiql, 'fields': fields[field]["Fields"]}
     url = firm["FIRM"][env] + fields[field]["URLPart"]
 
     response = requests.get(url, params=params, headers=headers)
 
     root = etree.fromstring(response.content)
-    print(root.tag)
 
     for child in root:
-        print(child.tag)
+        key = child.attrib.get('lxID')
         for grandchild in child:
-            print(grandchild.tag)
+            # if a tag ends in ID then it is a foreign key and might have an LxID
+            # if it has an LxID then we want that instead of the text
+            if grandchild.attrib.get('lxID') is not None:
+                data[key][grandchild.tag] = grandchild.attrib.get('lxID')
+            else:
+                data[key][grandchild.tag] = grandchild.text
 
-get_field("Contract", "TRAIN")
+    return data
+
+contract = get_field("Contract", "TRAIN")
+print(contract)
