76cd7e7845e50450eac0704acb907b79297eb475
adam <adam@bednar.co>
2017-12-04 22:13:01 -0500

    Added individual record get by LxID

--- lucernex.py
+++ lucernex.py
@@ -33,6 +33,30 @@ def fiql_get(field, env="TRAIN", fiql=""):
 
     return data
 
+def lxid_get(field, lxid, env="TRAIN"):
+    data = defaultdict(dict)
+    lxid = "68701"
+
+    headers = {'Authorization': firm["FIRM"]["Token"], 'Content-Type': 'application/xml'}
+    params = {'deep': 'true', 'wantClientID': 'true', 'wantLxID':'true'}
+    url = firm["FIRM"][env] + "/rest/businessObject/CustomCodeField/lxid/" + lxid
+
+    response = requests.get(url, params=params, headers=headers)
+
+    print(response.text)
+
+    root = etree.fromstring(response.content)
+
+    for child in root:
+        if child.attrib.get('lxID') is not None:
+            data[child.tag] = {'RecID': child.attrib.get('lxID'), 'Name': child.text}
+        else:
+            data[child.tag] = child.text
+
+    return data
+
 
 def get_radius_unit(env="TRAIN"):
     return fiql_get("CustomCodeField", env, "CustomCodeTableID==Radius Unit")
+
+print(lxid_get("nothing"))
